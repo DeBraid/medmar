adaa47d89bff7ddc7f2cdba079c9dee7cce3d5a1
DeBraid <de.braid@gmail.com>
2014-07-04 16:15:36 -0400

    bootstrap 3 in, removed server code

--- medmar.js
+++ medmar.js
@@ -1,20 +1,9 @@
 if (Meteor.isClient) {
-  // Prods = new Meteor.Collection("prods");
-
-  Template.main.producers = function () {
+  
+  Template.table.producers = function () {
     
     return Prods.find();
-  }
-}
 
-if (Meteor.isServer) {
-  
-  Meteor.startup(function () {
-    // Prods = new Meteor.Collections('prods');
+  };
 
-
-  //  Template.main.prods = function(){
-  //   return Prods.find().fetch().map(function(it){ return it.Name; });
-  // };
-  });
 }
